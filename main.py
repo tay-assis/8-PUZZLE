ccc398b75cf619761eec4e185ef7e7c395255996
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2024-08-20 22:15:49 -0300

    geração do estado sussesor

--- main.py
+++ main.py
@@ -3,23 +3,49 @@ import os
 
 # Definição da classe Estado
 class Estado:
-    def __init__(self, matriz_inicial):
+    def __init__(self,  estado_anterior, movimento): 
         """Inicializa o estado com a matriz 3x3."""
-        self.matriz = matriz_inicial
-
+        
+        if self.validar_movimento(estado_anterior, movimento):
+            self.matriz = self.gerar_sucessor(estado_anterior,movimento)
+            
     def embaralhar(self):
         """Embaralha o estado inicial de forma que seja resolvível."""
         # Implementação futura
         pass
 
-    def validar_movimento(self, movimento):
+    def validar_movimento(self,movimento):
         """Valida se um movimento é possível."""
         # Implementação futura
+        return True
         pass
 
-    def gerar_sucessor(self, movimento):
+    def gerar_sucessor(self,estado_anterior,movimento):
         """Gera um novo estado a partir de um movimento válido."""
-        # Implementação futura
+        novo_estado = estado_anterior
+        row = 0
+        col = 0
+        # Encontrar a posição do zero
+        for i in range(3):
+            for j in range(3):
+                if estado_anterior[i][j] == 0:
+                    row = i
+                    col = j
+        # atualizar a matriz
+        if movimento == "w":
+            novo_estado[row][col] = estado_anterior[row+1][col]
+            novo_estado[row+1][col] = 0
+        if movimento == "s":
+            novo_estado[row][col] = estado_anterior[row-1][col]
+            novo_estado[row-1][col] = 0
+        if movimento == "a":
+            novo_estado[row][col] = estado_anterior[row][col+1]
+            novo_estado[row][col+1] = 0
+        if movimento == "d":
+            novo_estado[row][col] = estado_anterior[row][col-1]
+            novo_estado[row][col-1] = 0
+            
+        return novo_estado
         pass
 
     def avaliar_jogo(self):
@@ -48,7 +74,7 @@ def main():
     """Executa o fluxo principal do jogo."""
     estado_inicial = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]  # Exemplo de matriz inicial
 
-    estado = Estado(estado_inicial)  # Instancia a classe Estado
+    estado = Estado(estado_inicial,"d")  # Instancia a classe Estado
 
     interface = InterfaceUsuario()  # Instancia a classe InterfaceUsuario
     interface.iniciar_jogo()
